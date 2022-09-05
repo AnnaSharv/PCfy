bddac708ee7d8259b20eb4fa86e821cbf3235976
unknown <anasharvadze93@gmail.com>
2022-09-05 23:39:00 +0400

    Updates

---
+++ precache-manifest.03b3fa2fcbf3a94719cee0572687b1d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b80acf9dbc4ef70dbb5",
+    "url": "/PCfy/static/css/main.6ec91933.chunk.css"
+  },
+  {
+    "revision": "4b80acf9dbc4ef70dbb5",
+    "url": "/PCfy/static/js/main.4b80acf9.chunk.js"
+  },
+  {
+    "revision": "d7979459db3fa557c29a",
+    "url": "/PCfy/static/js/1.d7979459.chunk.js"
+  },
+  {
+    "revision": "0fc25bc409cbf6d963ab",
+    "url": "/PCfy/static/js/runtime~main.0fc25bc4.js"
+  },
+  {
+    "revision": "324bd5d5fd7e275ccba135cc54f2efba",
+    "url": "/PCfy/static/media/Vector.324bd5d5.svg"
+  },
+  {
+    "revision": "bf266927a252d203598b28b802acaedc",
+    "url": "/PCfy/static/media/logo.bf266927.svg"
+  },
+  {
+    "revision": "b9f0ed0ba30d3fae9f1c4fd53145ed8d",
+    "url": "/PCfy/static/media/landing-img.b9f0ed0b.svg"
+  },
+  {
+    "revision": "a32958e311e73a999baa5d2db6a0df60",
+    "url": "/PCfy/static/media/confetti.a32958e3.svg"
+  },
+  {
+    "revision": "62fe69ae1507a7ddfd359869fcff37a6",
+    "url": "/PCfy/static/media/logo-round.62fe69ae.svg"
+  },
+  {
+    "revision": "870c326af0ad3a04d346508fa889982e",
+    "url": "/PCfy/index.html"
+  }
+];
